593b75d12de8f753ae38ef96aae26d8e7973a0c9
Dimeygee <untamed737@gmail.com>
2022-10-28 17:10:22 +0100

    avatae component created

---
+++ src/components/avatar.jsx
@@ -0,0 +1,12 @@
+
+
+export const  Avatar = () => {
+
+    return (
+        <div id="avatar">
+            <div id="profile__img" ></div>
+            <p>Annette Black</p>
+        </div>
+    )
+
+}
