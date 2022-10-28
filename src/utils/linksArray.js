161beb90ab7a5aca139f4c66958dc0e975137c4b
Dimeygee <untamed737@gmail.com>
2022-10-28 17:15:57 +0100

    added  links array

---
+++ src/utils/linksArray.js
@@ -0,0 +1,10 @@
+
+
+export const LinksArray = [
+    {id: 1, name: "Twitter Link", link: ""},
+    {id: 2, name: "Zuri Team", link: ""},
+    {id: 3, name: "Zuri Books", link: ""},
+    {id: 4, name: "Python Books", link: ""},
+    {id: 5, name: "Background Check for Coders", link: ""},
+    {id: 5, name: "Design Books", link: ""},
+]
