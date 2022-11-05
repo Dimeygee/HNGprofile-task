c4c32c38086d1faa87ce23ee9be222bd1ed8d7c3
Dimeygee <untamed737@gmail.com>
2022-11-05 22:58:03 +0100

    added cistom validation to the contact form

--- src/components/forms.jsx
+++ src/components/forms.jsx
@@ -1,23 +1,43 @@
 
 
 
-export const Input = ({placeholder, id, type, label}) => {
+export const Input = ({placeholder, id, type, label, value,  change, error, setError}) => {
+    
+    const style = {
+        border: error ? "1px solid #F89687" : ""
+    }
+
+    const handleChange = e => {
+        change(e.target.value)
+        setError("")
+    }
 
     return (
         <div>
-            <label for={id}>{label}</label>
-            <input type={type} placeholder={placeholder} id={id} name={id} required />
+            <label htmlFor={id}>{label}</label>
+            <input type={type} value={value} placeholder={placeholder} style={style}  onChange={handleChange} id={id} name={id} />
+            <small style={{ color: "#F83F23", fontSize:"14px",fontWeight: 400  }}>{error}</small>
         </div>
     )
 
 }
 
-export const Textarea = ({placeholder, id, label}) => {
+export const Textarea = ({placeholder, id, label, value,  change, error, setError}) => {
+
+    const style = {
+        border: error ? "1px solid #F89687" : ""
+    }
+
+    const handleChange = e => {
+        change(e.target.value)
+        setError("")
+    }
 
     return (
         <div>
-            <label for={id}>{label}</label>
-            <textarea  placeholder={placeholder} id={id} name={id} required></textarea>
+            <label htmlFor={id}>{label}</label>
+            <textarea  placeholder={placeholder} style={style} value={value}  id={id} name={id} onChange={handleChange} ></textarea>
+            <small style={{ color: "#F83F23", fontSize:"14px",fontWeight: 400  }}>{error}</small>
         </div>
     )
 
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,8 +1,51 @@
 import { Input, Textarea } from "../components/forms"
 import { Footer } from "../components/footer"
+import { useState } from "react"
 
 
 export const Contact = () => {
+
+    const [first_name, setFirstName] = useState("")
+    const [last_name, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+    const [message, setMessage] = useState("")
+    const [isChecked, setChecked] = useState(false)
+
+    const [first_name_err, setFirstNameErr] = useState("")
+    const [last_name_err, setLastNameErr] = useState("")
+    const [email_err, setEmailErr] = useState("")
+    const [message_err, setMessageErr] = useState("")
+
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        if(first_name === ""){
+            setFirstNameErr("Firstname should not be blank")
+            
+        }
+
+        if(last_name === ""){
+            setLastNameErr("Lastname should not be blank")
+        }
+
+        if(email === ""){
+            setEmailErr("Email should not be blank")
+        }
+        if(message === ""){
+            setMessageErr("Message should not be blank")
+        }
+
+
+        if(first_name !== "" && last_name !== "" && email !== "" && message !== "" && isChecked !== true){
+            console.log("fine!!")
+            return
+        }
+
+
+    }
+
+    
 
     return (
         <div id="contact_outer_container">
@@ -11,22 +54,22 @@ export const Contact = () => {
                     <h3>Contact Me</h3>
                     <p>Hi there, contact me to ask me about anything you have in mind.</p>
                 </div>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="form_control_1">
-                        <Input id="first_name" placeholder="Enter your first name" type="text" label="First name" />
-                        <Input id="last_name" placeholder="Enter your last name" type="text" label="Last name" />
+                        <Input id="first_name" placeholder="Enter your first name" value={first_name}  type="text" label="First name"  error={first_name_err} change={setFirstName} setError={setFirstNameErr}  />
+                          <Input id="last_name" placeholder="Enter your last name" value={last_name} type="text" label="Last name" error={last_name_err}  change={setLastName}   setError={setLastNameErr}  />
                     </div>
                     <div className="form_control_2">
-                        <Input id="email" placeholder="yourname@email.com" type="email" label="Email" />
+                        <Input id="email" placeholder="yourname@email.com" value={email} type="email" label="Email" error={email_err} change={setEmail} setError={setEmailErr}    />
                     </div>
                     <div className="form_control_3">
-                        <Textarea id="message" placeholder="Send me a message and I'll reply you as soon as possible..."  label="Message" />
+                        <Textarea id="message" placeholder="Send me a message and I'll reply you as soon as possible..."  value={message} error={message_err} change={setMessage} setError={setMessageErr}  label="Message" />
                     </div>
                     <div className="agreement">
-                        <input type="checkbox" />
+                        <input type="checkbox"/>
                         <span>You agree to providing your data to D_normal_1 who may contact you.</span>
                     </div>
-                    <button id="btn__submit">Send message</button>
+                    <button id="btn__submit" >Send message</button>
                 </form>
             </div>
             <Footer />
